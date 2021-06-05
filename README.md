19cbb16bd107be517fa3a68effa60be4264dbcf5
Pallav <pallavmahamana@gmail.com>
2021-06-05 12:36:25 +0530

    added gitascraper and restructure

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # scrapers-and-spiders
 Just some web scraping scripts and spiders i wrote in my free time to automate boring stuff.
 
+### Newspapers Scrapers
+
 **thehindu** is a python script to download daily The Hindu Epaper in pdf format
 
 usage : ./thehindu dd/mm/yyyy    - Downloads the E-paper of dd/mm/yyyy in pdf format.
@@ -10,3 +12,10 @@ usage : ./thehindu               - Downloads the today newspaper.
 **indianexpress** is a python script to download todays indianexpress , it uses selenium, webkit2png and img2pdf
 
 usage: sudo ./indianexpress - Downloads todays Epaper in pdf format.
+
+
+
+### Gita Scraper
+
+**gitasitiscrape.py** is python scraper which scrape asitis.com to spit out a json file of whole gita
+
---
+++ gitascrape/gitasitiscrape.py
@@ -0,0 +1,73 @@
+# Author - Pallav Mahamana
+# this script scrapes gita from asitis.com and spits out a gitasitis.json file containing all shlokas
+# with translations and purports
+
+from colorama import Fore, Style # For colors in console stdout
+import requests, json
+from bs4 import BeautifulSoup # For parsing and scraping
+
+
+
+gitasitis = []
+
+# def create_page_url(chapter,text):
+# 	return URL.replace('CH',str(chapter)).replace('TXT',str(text))
+
+
+def scrape_shloka(url):
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content,'html.parser')
+	try:
+		shloka = soup.find("span",class_="Italic").text
+		print(Fore.YELLOW+shloka) #For Logging Purpose
+
+	except:
+		shloka = ""
+	try:
+		translation = soup.find("div",class_="Translation").text
+	except:
+		translation = ""
+	try:
+		purport = soup.find("div",class_="Purport").text
+	except:
+		purport = ""
+	try:
+		syn = soup.find("div",class_="Synonyms-SA").text.split(';')
+	except:
+		syn = ""
+	
+	print(Fore.GREEN+translation+'\n') #For Logging Purpose
+
+	gitasitis.append(
+		{"TEXT":url.split('/')[-1].rsplit('.html')[0],
+		"SHLOKA":shloka,
+		"SYN":syn,
+		"TRANSLATION":translation,
+		"PURPORT":purport
+		})
+
+
+gita = {}
+ch = 1	
+while(ch<19):
+	page = requests.get("https://asitis.com/"+str(ch))
+	soup = BeautifulSoup(page.content,'html.parser')
+	h4 = soup.findAll("h4")
+	for ele in h4:
+		if ele.find("a",href=True):
+			scrape_shloka(ele.find("a")['href'])
+	gita[ch] = gitasitis
+	gitasitis = []
+	ch = ch + 1
+	
+	# while scrape_shloka(ch,sh)!=Exception:
+	# 	sh = sh + 1
+	
+
+with open('gitasitis.json','w',encoding='utf-8') as fp:
+	json.dump(gita,fp)
+
+
+
+print(Style.RESET_ALL) # reset stdout colors
+
