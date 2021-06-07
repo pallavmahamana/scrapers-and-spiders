eb139dba865a73cec301b0cc5ca637af42f457fb
Pallav <pallavmahamana@gmail.com>
2021-06-07 11:51:52 +0530

    quote.sh has alias help

--- gitascrape/quote.sh
+++ gitascrape/quote.sh
@@ -1,6 +1,7 @@
 # This script use jq to get a random shloka from gitasitis.json file and print its shloka,translation,(purport..)
 # set the path of gitasitis.json file accordingly in RAN and TEXT variable
 #!/bin/bash
+# function quote(){                                           <--- uncomment this and line(18)
 RAN=`jq '.[0] | .[] | .[] | .TEXT' gitasitis.json | shuf | head -n 1`  # Pick a random TEXT
 TEXT=`jq '.[0] | .[] | .[]' gitasitis.json | jq "select(.TEXT==$RAN)"` # Read a random TEXT from jsonfile
 
@@ -14,3 +15,4 @@ echo $(tput setaf 3)$SHLOKA | tr -d \"
 echo $(tput setaf 2)$TRANSLATION | tr -d \"
 #uncomment below line to echo purport too
 #echo $(tput setaf 4)$PURPORT | tr -d \"
+#}             <-- uncomment this and line(4) to make a function and add it to .bashrc/.zshrc to make it alias
