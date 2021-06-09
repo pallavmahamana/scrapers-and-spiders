b73ba1f305c2782e838394ed5dc138831de8a855
Pallav <pallavmahamana@gmail.com>
2021-06-09 21:07:33 +0530

    added gita 0 <shloka num> option

--- gitascrape/gita.sh
+++ gitascrape/gita.sh
@@ -2,19 +2,33 @@
 # dependency(install): jq --> https://stedolan.github.io/jq
 # set the path of gitasitis.json file accordingly in RAN and TEXT variable
 # USAGE. quote - for random shloka
+# USAGE, quote 0 {TEXT_NUM} - for exact TEXT , eg. quote 0 2.47
 # USAGE, quote 1 - for random shloka and translation both
 # USAGE, quote 2 - for random shloka, translation and purport
 #!/bin/bash
-#function quote(){                                           #<--- uncomment this and line(36)
+#function gita(){                                           #<--- uncomment this and line(50)
 RAN=`jq '.[0] | .[] | .[] | .TEXT' gitasitis.json | shuf | head -n 1`  # Pick a random TEXT
 TEXT=`jq '.[0] | .[] | .[]' gitasitis.json | jq "select(.TEXT==$RAN)"` # Read a random TEXT from jsonfile
 
 SHLOKA=`echo  $TEXT | jq -r '.SHLOKA'`
 
+
 if [ $# -eq 0 ]
 then
 	echo $(tput setaf 6)BG $RAN | tr -d \"
 	echo $(tput setaf 3)$SHLOKA
+elif [ $1 -eq 0 ]
+then
+	TEXT=`jq '.[0] | .[] | .[]' gitasitis.json | jq "select(.TEXT==\"${2}\")"`
+	SHLOKA=`echo  $TEXT | jq -r '.SHLOKA'`
+	TRANSLATION=`echo $TEXT | jq -r '.TRANSLATION'`
+	PURPORT=`echo $TEXT | jq -r '.PURPORT'`
+	echo $(tput setaf 6)BG $2 | tr -d \"
+	echo $(tput setaf 3)$SHLOKA
+	echo
+	echo $(tput setaf 2)$TRANSLATION
+	echo
+	echo $(tput setaf 172)$PURPORT
 elif [ $1 -eq 1 ]
 then
 	TRANSLATION=`echo $TEXT | jq -r '.TRANSLATION'`
@@ -33,4 +47,4 @@ then
 	echo
 	echo $(tput setaf 172)$PURPORT
 fi
-#}             <-- uncomment this and line(8) to make a function and add it to .bashrc or .zshrc to make it alias
+#}             <-- uncomment this and line(9) to make a function and add it to .bashrc or .zshrc to make it alias
